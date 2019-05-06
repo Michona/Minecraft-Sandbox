07df3158ad422f87cd01c5672c7d4a223b11b47b
Michona <mitevski1201miki@gmail.com>
2019-05-06 10:43:59 +0200

    Adds jobs to handle box colliders spawning.Adds systems for player movement sync

---
+++ Assets/Scripts/Systems/MovementSyncSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+public class MovementSyncSystem : ComponentSystem
+{
+
+    EntityQuery query;
+
+    protected override void OnCreate()
+    {
+        query = GetEntityQuery(
+            ComponentType.ReadOnly<Transform>(),
+            ComponentType.ReadWrite<Translation>(),
+            ComponentType.ReadOnly<PlayerTag>());
+    }
+    protected override void OnUpdate()
+    {
+        Entities.With(query).ForEach(
+            (Entity entity, Transform transform, ref Translation translation) => {
+                translation.Value = transform.position;
+            });
+    }
+}
